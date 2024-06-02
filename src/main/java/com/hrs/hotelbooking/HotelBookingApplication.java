86dde4d0cf993a51fa166cd24317bec878014191
HuyAtreides <phangiahuy2001@gmail.com>
2024-06-02 21:07:44 +0700

    initial commit

---
+++ src/main/java/com/hrs/hotelbooking/HotelBookingApplication.java
@@ -0,0 +1,14 @@
+package com.hrs.hotelbooking;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class HotelBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingApplication.class, args);
+	}
+}
